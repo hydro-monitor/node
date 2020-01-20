3f5c580ac741761c04cb21d1435f188b855f7dc3
Agustina Barbetta <agustina.barbetta@gmail.com>
2020-01-20 14:13:05 -0300

    Updated test

--- cmdtest/test.go
+++ cmdtest/test.go
@@ -1,30 +1,27 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/golang/glog"
 	"github.com/hydro-monitor/node/pkg/server"
-	"io"
-	"io/ioutil"
-	"mime/multipart"
-	"net/http"
-	"os"
-	"path/filepath"
-	"time"
 )
 
 const (
-	postNodeMeasurementUrl = "http://localhost:8080/api/nodes/%s/readings"
-	NODE_NAME              = "lujan-1"
+	postNodeMeasurementUrl = "http://antiguos.fi.uba.ar:443/api/nodes/%s/readings"
+	NODE_NAME              = "1"
 )
 
 var client = &http.Client{
 	Timeout: 10 * time.Second,
 }
 
-func makeReq(uri string, params map[string]string, path string) (*http.Response, error) {
-	file, err := os.Open(path)
+/*
+func makeReq(uri string, m server.APIMeasurement) (*http.Response, error) {
+	picturePath := m.Picture
+
+	file, err := os.Open(picturePath)
 	if err != nil {
 		return nil, err
 	}
@@ -32,35 +29,30 @@ func makeReq(uri string, params map[string]string, path string) (*http.Response,
 
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
-	if err := writer.WriteField("timestamp", time.Now().Format(time.RFC3339)); err != nil {
+
+	if err := writer.WriteField("timestamp", m.Time.Format(time.RFC3339)); err != nil {
 		return nil, err
 	}
-	if err := writer.WriteField("waterLevel", fmt.Sprintf("%f", 3.14)); err != nil {
+	if err := writer.WriteField("waterLevel", fmt.Sprintf("%f", m.WaterLevel)); err != nil {
 		return nil, err
 	}
 
-	part, err := writer.CreateFormFile("picture", filepath.Base(path))
+	part, err := writer.CreateFormFile("picture", filepath.Base(picturePath))
 	if err != nil {
 		return nil, err
 	}
 	if _, err := io.Copy(part, file); err != nil {
 		return nil, err
 	}
-
-	/*for key, val := range params {
-		_ = writer.WriteField(key, val)
-	}*/
-
 	if err := writer.Close(); err != nil {
 		return nil, err
 	}
-    contentType := writer.FormDataContentType()
-    glog.Info(contentType)
+
+	contentType := writer.FormDataContentType()
 	res, err := http.Post(fmt.Sprintf(uri, NODE_NAME), contentType, body)
 	if err != nil {
 		return nil, err
 	}
-	//req.Header.Set("Content-Type", writer.FormDataContentType())
 	return res, err
 }
 
@@ -68,10 +60,10 @@ func main() {
 	m := server.APIMeasurement{
 		Time:       time.Now(),
 		WaterLevel: 600,
-		Picture:    "/Users/mporto/Desktop/maxresdefault.jpg",
+		Picture:    "/Users/abarbetta/Desktop/lala.jpg",
 	}
 
-	res, err := makeReq(postNodeMeasurementUrl, nil, m.Picture)
+	res, err := makeReq(postNodeMeasurementUrl, m)
 	if err != nil {
 		glog.Errorf("Error making req: %v", err)
 		return
@@ -84,16 +76,22 @@ func main() {
 		return
 	}
 	bodyString := string(bodyBytes)
-
 	glog.Infof("Status code: %d. Body: %v", res.StatusCode, bodyString)
+	return
+}
+*/
 
-	/*
-		resp, err := client.Do(request)
-		if err != nil {
-			glog.Errorf("Error making request: %v", err)
-			return
-		}
-		defer resp.Body.Close()
-	*/
+func main() {
+	m := server.APIMeasurement{
+		Time:       time.Now(),
+		WaterLevel: 600,
+		Picture:    "/Users/abarbetta/Desktop/lala.jpg",
+	}
+
+	err := server.PostNodeMeasurement(m)
+	if err != nil {
+		glog.Errorf("Error making req: %v", err)
+		return
+	}
 	return
 }
