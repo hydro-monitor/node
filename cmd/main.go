316328df1757c2e93a15f2d9c23bbcf4f1221dcb
Agustina Barbetta <agustina.barbetta@gmail.com>
2020-02-03 22:41:21 -0300

    More debug logs

--- cmd/main.go
+++ cmd/main.go
@@ -40,6 +40,9 @@ func newNode(triggerMeasurer, triggerAnalyzer, triggerConfig, manualMeasurer cha
 	env := envconfig.New()
 	glog.Infof("Env config: %v", env)
 
+	env = envconfig.New()
+	glog.Infof("Env config 2: %v", env)
+
 	return &node{
 		t:  trigger.NewTrigger(env.InitialTriggerInterval, triggerMeasurer, triggerAnalyzer, wg),
 		m:  measurer.NewMeasurer(triggerMeasurer, manualMeasurer, measurerAnalyzer, wg),
@@ -69,6 +72,9 @@ func main() {
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	env := envconfig.New()
+	glog.Infof("Env config 3: %v", env)
 
 	glog.Info("Awaiting signal")
 	sig := <-sigs
