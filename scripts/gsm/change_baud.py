30a2d370e52f028a98b3cdafa4cb70e4c30d77e3
Agustina Barbetta <agustina.barbetta@gmail.com>
2020-03-24 12:37:49 -0300

    Update GSM scripts with new baudrate

--- scripts/gsm/change_baud.py
+++ scripts/gsm/change_baud.py
@@ -2,11 +2,11 @@ import serial
 import os, time
 
 # Enable Serial Communication
-port = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
+port = serial.Serial("/dev/ttyS0", baudrate=19200, timeout=1)
 
 # Transmitting AT Commands to the Modem
 # '\r\n' indicates the Enter key
 
-port.write('AT'+'\r\n')
+port.write('AT+IP=115200'+'\r\n')
 rcv = port.read(10)
 print rcv
